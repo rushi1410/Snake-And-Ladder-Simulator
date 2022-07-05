18df174af2bdb6f48be1df19594d667edbab049e
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-05 23:25:36 +0530

    Added:Player rolls die to get a number between 1 to 6

---
+++ src/com/bridgelabz/RollsDie_UC2.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class RollsDie_UC2 {
+
+	public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder game.");
+
+           int startPosition=0;
+           int DieRolled = (int) (Math.random()*6)+1;
+           System.out.println(DieRolled);
+    }
+	}
+
+

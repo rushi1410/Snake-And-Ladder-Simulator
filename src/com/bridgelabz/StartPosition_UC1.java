83cece623f563f58244bff0fbbf2c409becfd39c
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-05 23:18:26 +0530

    Added:Single Player at Start Position zero

---
+++ src/com/bridgelabz/StartPosition_UC1.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class StartPosition_UC1 {
+	public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder game.");
+
+           int startPosition=0;
+           System.out.println(" startpostion zero ");
+    }
+
+}

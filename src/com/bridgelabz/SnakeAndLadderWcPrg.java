f63090cb9bc532dff2de146e15a5dbae594bc2c4
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-05 23:10:35 +0530

    Added:Welcome to Snake And Ladder Game

---
+++ src/com/bridgelabz/SnakeAndLadderWcPrg.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class SnakeAndLadderWcPrg {
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to Snake And Ladder Game");
+		}
+		}		  
+	

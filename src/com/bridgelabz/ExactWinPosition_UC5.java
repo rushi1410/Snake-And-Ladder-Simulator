8b45f620704994d91645921acb125fd3adbb91bf
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-05 23:51:45 +0530

    Added:Player gets exact win position 100

---
+++ src/com/bridgelabz/ExactWinPosition_UC5.java
@@ -0,0 +1,42 @@
+package com.bridgelabz;
+
+public class ExactWinPosition_UC5 {
+	public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder game.");
+
+        int Position = 0;
+        int newPosition = 0;
+        int WinPosition = 100;
+
+        while (Position < WinPosition) {
+            int DieRolled = (int) (Math.random() * 6) + 1;
+            int checkOption = (int) (Math.random() * 3);
+
+            switch (checkOption) {
+                case NO_PLAY:
+                    newPosition = 0;
+                    break;
+                case LADDER:
+                    newPosition = DieRolled;
+                    break;
+                case SNAKE:
+                    newPosition = -DieRolled;
+            }
+            Position = Position + newPosition;
+            if (Position < 0) {
+                Position = 0;
+            }
+            if (Position>100){
+                Position=newPosition;
+            }
+            System.out.println("You are now at " + Position + "th" + " position");
+        }
+    }
+
+}
+
+
+
